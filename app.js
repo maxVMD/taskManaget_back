2842421dc1537da3d1e5ca709b14dcb3eeea8ee0
Макс <maxVMDby@mail.ru>
2020-09-28 09:30:13 +0300

    minor fixes

--- app.js
+++ app.js
@@ -24,12 +24,13 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Methods", "*");
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     // res.setHeader('Access-Control-Allow-Credentials', true);
